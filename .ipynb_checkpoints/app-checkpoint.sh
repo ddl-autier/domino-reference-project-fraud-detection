be74f8f3919668261f651778d8df3f8cd31e846a
Guillaume Autier <guillaume.autier@dominodatalab.com>
2022-04-13 05:18:55 -0700

    Added app bees fixes (to delete later)
    Pushed from Domino: https://demo2.dominodatalab.com/workspace/guillaume_autier/Fraud_Detection?executionId=624eae444906b439238b5847

--- .ipynb_checkpoints/app-checkpoint.sh
+++ .ipynb_checkpoints/app-checkpoint.sh
@@ -15,8 +15,8 @@
 #pip install -r requirements_apps.txt --user
 #python app-dash.py
 
-pip install dash==2.1.0 --user
-pip install jupyter-dash==0.4.1 --user
+pip install dash==2.0.0 --user
+#pip install jupyter-dash==0.4.1 --user
 python app-bees.py
 
 
